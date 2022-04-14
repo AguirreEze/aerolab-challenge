6fb11bcfde98701f6658cda409aeb7b7ddf6f79d
AguirreEze <aguirre.eze95@gmail.com>
2022-04-14 18:04:38 -0300

    Feat moved context

--- components/Store/index.tsx
+++ components/Store/index.tsx
@@ -1,32 +1,22 @@
 import StoreElement from "components/Product"
 import { StoreNav } from "components/StoreNav"
-import { initialStore, StoreContext } from "context/store"
-import { useEffect, useState } from "react"
-import { getProducts } from "services/products"
-import { StoreType, Order } from "types"
+import { StoreContext } from "context/store"
+import { useContext } from "react"
+
 import styles from "./styles.module.scss"
 
 export default function Store() {
-  const [store, setStore] = useState<StoreType>(initialStore)
-  const [page, setPage] = useState<number>(1)
-  const [order, setOrder] = useState<Order>(Order.MOSTRECENT)
+  const { store } = useContext(StoreContext)
 
-  useEffect(() => {
-    getProducts(page, order).then(setStore)
-  }, [page, order])
   return (
     <main className={styles.background}>
-      <StoreContext.Provider value={{ store, page, setPage, order, setOrder }}>
-        <StoreNav bottomNav={false} />
-      </StoreContext.Provider>
+      <StoreNav bottomNav={false} />
       <ul className={styles.stand}>
         {store?.list.map((elem) => (
           <StoreElement key={elem._id} data={elem} />
         ))}
       </ul>
-      <StoreContext.Provider value={{ store, page, setPage, order, setOrder }}>
-        <StoreNav bottomNav={true} />
-      </StoreContext.Provider>
+      <StoreNav bottomNav={true} />
     </main>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,31 @@ import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { UserContext } from "context/user"
 import { useEffect, useState } from "react"
-import { UserType } from "types"
+import { Order, StoreType, UserType } from "types"
 import { getUser } from "services/user"
+import { initialStore, StoreContext } from "context/store"
+import { getProducts } from "services/products"
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [user, setUser] = useState<UserType | null>(null)
+
+  const [store, setStore] = useState<StoreType>(initialStore)
+  const [page, setPage] = useState<number>(1)
+  const [order, setOrder] = useState<Order>(Order.MOSTRECENT)
+
+  useEffect(() => {
+    getProducts(page, order).then(setStore)
+  }, [page, order])
+
   useEffect(() => {
     getUser().then(setUser)
   }, [])
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
-      <Component {...pageProps} />
+      <StoreContext.Provider value={{ store, page, setPage, order, setOrder }}>
+        <Component {...pageProps} />
+      </StoreContext.Provider>
     </UserContext.Provider>
   )
 }
